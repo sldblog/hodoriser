affcd367a3aeff268e138865b4de97dfc2f4268a
David Lantos <david.lantos@gmail.com>
2013-07-05 23:29:40 +0100

    Image source and encoding support.

--- hodor.rb
+++ hodor.rb
@@ -1,35 +1,38 @@
+#encoding: utf-8
 require 'rest-client'
 require 'nokogiri'
 require 'uri'
 
 module Hodor
   class Hodor
-    def hodor(url)
+    def initialize(url)
       parts = URI.split(url)
-      base = parts[0] + '://' + parts[2]
+      @base = parts[0] + '://' + parts[2]
+      @doc = Nokogiri::HTML(RestClient.get(url))
+      @source_charset = (@doc.xpath('//meta/@charset').first || 'UTF-8').to_s
+    end
 
-      doc = Nokogiri::HTML(RestClient.get(url))
-      doc.xpath('//div|//span|//img/@alt').each { |node| node_hodor node }
-      doc.xpath('//script/@src|//link/@href').each { |node| src_hodor(node, base) }
-      # TODO: single element values like <span>text</span>, <input value="default"/>
-      doc.to_xml(:indent => 2)
+    def hodor
+      @doc.xpath('//div|//span|//img/@alt').each { |node| node_hodor node }
+      @doc.xpath('//script/@src|//img/@src|//link/@href').each { |node| src_hodor node }
+      @doc.to_xml(:indent => 2)
     end
 
     def node_hodor(node)
-      node.content = hodor!(node.content) if node.text?
+      node.content = make_hodor(node.content) if node.text?
       node.children.each { |child| node_hodor child }
     end
 
-    def src_hodor(attr, base)
-      attr.content = base + '/' + attr unless attr.content =~ /^http/
+    def src_hodor(attr)
+      attr.content = @base + '/' + attr unless attr.content =~ /^http/
     end
 
-    def hodor!(not_hodor)
-      not_hodor.
+    def make_hodor(not_hodor)
+      not_hodor.encode('UTF-8', @source_charset, {:invalid => :replace}).
         gsub(/\p{Ll}\p{Alpha}*/, 'hodor').
         gsub(/\p{Lu}\p{Alpha}*/, 'Hodor')
     end
   end
 end
 
-puts Hodor::Hodor.new.hodor ARGV.first
+puts Hodor::Hodor.new(ARGV.first).hodor
